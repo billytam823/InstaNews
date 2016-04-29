8394c89fda66e20f76173ce60fa75d3b2c46d53b
Billy <billytam823@gmail.com>
2016-04-29 23:08:19 +0000

    Adding paragraph to content area and styling it

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,20 @@ var $content = $('.content');
 $(document).ready(function(){ // When Document is Ready
   $('select').on('change', function(){ // On Dropdown Button Change
 
+    $('.header').addClass("minimized"); // Add class to shrink header
+    
+    // Grabs Category value from Dropdown Button
     var category = $('.category :selected').val();
-    console.log(category);
+
+    // Shows Loader
+    $content.append('<div class="loader"><img src="assets/images/ajax-loader.gif"></div>');
 
     $.getJSON( // NYT API for Article List
       'http://api.nytimes.com/svc/topstories/v1/'+ category +'.json?api-key=f157b849d96a2a6c75cf671b868049fd:9:75124091'
       )
+
     .done(function(data){ // After List is done Retrieving
 
-      $('.header').addClass("minimized"); // Add class to shrink header
       $content.children().remove(); // Remove previous content
 
       var newsItems = " ";
@@ -19,11 +24,17 @@ $(document).ready(function(){ // When Document is Ready
 
       for(i=0; articleNum < 12; i++){ //Pulling the info from JSON 12 times
 
+        //Checks if Multimedia section is empty
         if(data.results[i].multimedia !== ""){
         console.log(data.results[i].multimedia[2].url);
-        newsItems += '<div class="article outter-square"><div class="article-bg inner-square"><p><img class="article-image" src=\''+ data.results[i].multimedia[4].url +'\'></p></div></div>'
+        newsItems += '<div class="article outter-square">';
+        newsItems += '<a class="article-bg inner-square" href="'+ data.results[i].url +'" target="_blank"';
+        newsItems += 'style="background-image:url(\'' + data.results[i].multimedia[4].url + '\')">';
+        newsItems += '<p class="abstract">' + data.results[i].abstract + '</p>';
+        newsItems += '</a>';
+        newsItems += '</div>';
         
-        articleNum++; //Keep tracks of how many article with images
+        articleNum++; //Keep tracks of how many articles with images
 
         }
 
